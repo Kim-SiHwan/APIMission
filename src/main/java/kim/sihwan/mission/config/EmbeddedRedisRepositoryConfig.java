2b4588ce2880214912e05923a07b81acd1caf9c8
Kim-SiHwan <tjdans4166@naver.com>
2021-08-31 17:46:16 +0900

    refactor: Remote Redis -> EmbeddedRedis 변경 ( git clone 후 로컬 레디스 or 도커를 설치해야하는 번거로움을 없앨 수 있음. 서비스를 확장하게 된다면 Remote로 다시 변경 후 Docker compose로 구현할 예정 )

--- src/main/java/kim/sihwan/mission/config/EmbeddedRedisRepositoryConfig.java
+++ src/main/java/kim/sihwan/mission/config/EmbeddedRedisRepositoryConfig.java
@@ -1,6 +1,5 @@
 package kim.sihwan.mission.config;
 
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -8,12 +7,10 @@ import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
-import org.springframework.data.redis.serializer.StringRedisSerializer;
 
-@Slf4j
 @Configuration
 @EnableRedisRepositories
-public class RedisRepositoryConfiguration {
+public class EmbeddedRedisRepositoryConfig {
     @Value("${spring.redis.host}")
     private String redisHost;
 
@@ -29,7 +26,6 @@ public class RedisRepositoryConfiguration {
     public RedisTemplate<?, ?> redisTemplate() {
         RedisTemplate<byte[], byte[]> redisTemplate = new RedisTemplate<>();
         redisTemplate.setConnectionFactory(redisConnectionFactory());
-        redisTemplate.setKeySerializer(new StringRedisSerializer());
         return redisTemplate;
     }
 }
